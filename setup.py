46b0d21f30134d1647c3d71372add709782c992a
Hauke Heeren <heeren@match.uni-hannover.de>
2025-05-27 14:30:28 +0200

    Refactor setup.py to use setuptools and enhance package metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['match_lib'],
+setup(
+    name='match_lib_package',
+    version='0.1.0',
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    install_requires=[
+        # Add any dependencies here, e.g., 'numpy'
+    ],
+    author='Hauke Heeren',
+    author_email='heeren@match.uni-hannover.de',
+    description='A Python library for match robots and other things, integrated with ROS.',
+    license='MIT',
+    keywords='ROS Python match robots',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
 )
-
-setup(**setup_args)
