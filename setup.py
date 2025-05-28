9e582c8edb51d5ae01170db3e9b044cc68934010
pumablattlaus <heeren@match.uni-hannover.de>
2025-05-28 14:23:16 +0200

    couldnt find modules in python when building with ROS

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+# fetch values from package.xml
 setup(
-    version="0.1.0"
+    name='match_lib_package',
+    version="0.1.0",
+    package_dir={'': 'src'},
+    packages=find_packages("src"),
+    install_requires=[
+        'numpy'
+        # …
+    ],
 )
